227b3ea9a3fc6e4045d6dcb44fd8a0bdbf373078
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-03-31 17:16:57 +0900

    init

---
+++ src/main/java/com/boilerplate/api/Application.java
@@ -0,0 +1,34 @@
+package com.boilerplate.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+import java.util.TimeZone;
+
+@Slf4j
+@SpringBootApplication
+@RequiredArgsConstructor
+public class Application implements ApplicationListener<ApplicationReadyEvent> {
+    private final Environment environment;
+
+    public static void main(String[] args) {
+        init();
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        log.info("applicationReady status" + Arrays.toString(environment.getActiveProfiles()));
+    }
+
+    private static void init() {
+        log.info("Spring Server TimeZone : Asia/Seoul");
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
---
+++ src/test/java/com/boilerplate/api/BoilerPlateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilerplate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerPlateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
