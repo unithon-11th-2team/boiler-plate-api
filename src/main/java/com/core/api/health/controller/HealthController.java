5a8d52e54f9902600882e28700678af94530cea4
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2024-04-02 09:47:11 +0900

    fix: adapt log config

--- src/main/java/com/core/api/health/controller/HealthController.java
+++ src/main/java/com/core/api/health/controller/HealthController.java
@@ -2,6 +2,8 @@ package com.core.api.health.controller;
 
 import com.core.api.common.dto.ResponseDto;
 import com.core.api.health.dto.response.HealthResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
+@Tag(name = "Health Check")
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class HealthController {
     private final Environment environment;
-
+    
+    @Operation(summary = "health check")
     @GetMapping("/health")
     public ResponseEntity<ResponseDto<HealthResponse>> health() {
         var response = new HealthResponse(
